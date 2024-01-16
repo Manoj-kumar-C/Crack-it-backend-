8e4e2975d0e9b9cbcc5b85dd8b0f0459c4e3802e
manoj-chandran <developermanoj2003@gmail.com>
2024-01-16 20:02:51 +0530

    api update

--- index.js
+++ index.js
@@ -10,9 +10,37 @@ const songs = [
     { id: 4, title: 'Song 4', audioFile: 'song4.mp3' },
   ];
 
+
+
+
+const animals = [
+    { id: 1, title: 'Song 1', audioFile: 'https://github.com/Manoj-kumar-C/Storage-of-Meme-App/releases/download/0.1/1.mp3' },
+    { id: 2, title: 'NO god plz no', audioFile: 'https://github.com/Manoj-kumar-C/Storage-of-Meme-App/releases/download/0.1/10.mp3' },
+    { id: 3, title: 'Song 3', audioFile: 'song3.mp3' },
+    { id: 4, title: 'Song 4', audioFile: 'song4.mp3' },
+  ];
+
+
+  const popular = [
+    { id: 1, title: 'Song 1', audioFile: 'https://github.com/Manoj-kumar-C/Storage-of-Meme-App/releases/download/0.1/1.mp3' },
+    { id: 2, title: 'NO god plz no', audioFile: 'https://github.com/Manoj-kumar-C/Storage-of-Meme-App/releases/download/0.1/10.mp3' },
+    { id: 3, title: 'Song 3', audioFile: 'song3.mp3' },
+    { id: 4, title: 'Song 4', audioFile: 'song4.mp3' },
+  ];
+
 app.get('/', (req, res)=>{ 
     res.json(songs);
     console.log("Works Fine")
+})
+
+app.get('/animals', (req, res)=>{ 
+  res.json(animals);
+  console.log(" Animal Works Fine")
+})
+
+app.get('/popular', (req, res)=>{ 
+  res.json(popular);
+  console.log("Popular Works Fine")
 })
 
 app.listen(3000, ()=> {console.log("Works at 300")})
